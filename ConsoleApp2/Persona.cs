b3902fc60dab403fd7f312bc01cd3a038f3d4536
yaviracangie <aer.pacas@yavirac.edu.ec>
2021-06-22 11:23:03 -0600

    ejercicios en clase

---
+++ ConsoleApp2/Persona.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleA
+{
+    class Persona
+    {
+        string nombre;
+        string apellido;
+        int edad;
+        int peso;
+        float nota;
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Apellido { get { return apellido; } set { apellido = value; } }
+        public int Edad { get { return edad; } set { edad = value; } }
+        public int Peso { get { return peso; } set { peso = value; } }
+        public float Nota { get { return nota; } set { nota = value; } }
+
+        public string NombreCompleto
+        {
+            get { return $"{this.Nombre} {this.Apellido}"; } //interpolacion
+        }
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+
+        public string Saludar()
+        {
+            return $"Hola... como estas?, mi nombre es {this.Nombre}";
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleA
+{
+    class Program
+    {
+        static void Main(string[] datosPersonas)
+        {
+            Console.WriteLine("Escribe tu nombre: ");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu apellido: ");
+            var apellido = Console.ReadLine();
+
+            Console.WriteLine("Escribe tu peso: ");
+            var peso = int.Parse(Console.ReadLine());
+
+            //ToDo:Sebastian, debo leer 10 datos de una persona utilizando todos esos tipos de datos
+            ///float
+            //double
+            //DateTime // System.DateTime
+            //bool
+
+            Console.WriteLine("Escribe tu edad: ");
+            var edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe tu nota: ");
+            var nota = float.Parse(Console.ReadLine());
+
+            var amigo = new Persona
+            {
+                Nombre = nombre,
+                Apellido = apellido,
+                Peso = peso,
+                Edad = edad,
+                Nota = nota
+            };
+
+
+            Console.WriteLine($"Hola {amigo.Nombre}");
+            Console.WriteLine($"Tu apellido es: {amigo.Apellido}");
+            Console.WriteLine($"Tu edad es: {amigo.Edad}");
+            Console.WriteLine($"Tu peso es: {amigo.Peso}");
+            Console.WriteLine($"Tu nota es: {amigo.Nota}");
+
+            Persona vecina = new("Julia", "RioFrio");
+            Console.WriteLine($"Mi vecina se llama {vecina.NombreCompleto}");
+
+            Console.WriteLine("Escribe cualquier letrapara terminar");
+
+            Console.Read();
+
+        }       
+    }
+}
